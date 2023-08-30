97f2f1b5cb996de9c9128b2ccd18515ad64bdf00
Steve Schlepphorst <steve.schlepphorst@gmail.com>
2023-08-30 12:36:28 -0500

    Update Project.jsx

--- client/components/Project.jsx
+++ client/components/Project.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-function Project() {
-  return (
-    <div className="sampleProject">
-      <h1>Project</h1>
-    </div>
-  );
-}
+//function Project() {
+//  return (
+//    <div className="sampleProject">
+//      <h1>Project</h1>
+//    </div>
+//  );
+//}
 
 // ---- TO DO LIST HERE ----
 
 // ---- Imports go here ----
-import React from 'react';
+//import React from 'react';
 
 // ---- Build & Export the Project component here ----
 const Project = () => {
