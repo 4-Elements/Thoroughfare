fbc5acb9bffe26a5831b31883390eb204672b686
Wes <wp3445@gmail.com>
2023-08-29 23:29:07 -0500

    added to Mentee, CurrentMenteeList, ProjectFeed, and MentorDashboard components

--- client/pages/mentorDashboard.jsx
+++ client/pages/mentorDashboard.jsx
@@ -1,9 +1,31 @@
+// ---- TO DO ----
+// Add query string to getMentees function
+// If backend has not built out request to get mentees, need to add that.  The functionality assumes the response will be an array of objects, with each object representing a mentee (and containing that mentees current projects, tasks, and chats)
+
+// ---- imports go here ----
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './mentorDashboard.scss';
+import CurrentMenteeList from '../components/CurrentMenteeList.jsx';
+
+// ---- build and export mentor dashboard here ----
 export default function mentorDash() {
+  const [currentMentees, setCurrentMentees] = useState([]);
+
+  const getMentees = () => {
+    fetch(queryString)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCurrentMentees(data);
+      });
+  };
+
+  // remove this - it is just for testing
+
   return (
-    <div className='mentorDash'>
-      <h1>mentor dashboard</h1>
+    <div className="mentorDash">
+      <h1>Mentor Dashboard</h1>
+      <CurrentMenteeList currentMentees={currentMentees} />
     </div>
   );
 }
