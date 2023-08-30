a0f79364388334212a9c6018ef759794378184b0
Wes <wp3445@gmail.com>
2023-08-30 08:58:22 -0500

    draft of ProjectFeed

--- client/components/ProjectFeed.jsx
+++ client/components/ProjectFeed.jsx
@@ -2,10 +2,28 @@
 
 // ---- imports go here ----
 import React from 'react';
+import Project from './Project.jsx';
 
 // ---- build & export project feed component here ----
 const ProjectFeed = (props) => {
-  <div>Current Projects</div>;
+  const lessonComponentArray = [];
+
+  const buildProjectFeed = () => {
+    props.lessonsAssigned.forEach((currentLesson) => {
+      lessonComponentArray.push(
+        <Project key={currentLesson.name} projectInfo={currentLesson} />
+      );
+    });
+  };
+
+  buildProjectFeed();
+
+  return (
+    <div>
+      <div>Current Projects</div>
+      {lessonComponentArray}
+    </div>
+  );
 };
 
 export default ProjectFeed;
