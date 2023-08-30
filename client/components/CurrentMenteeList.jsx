c8e6838cfa58e095722bb58a7c52c45848b1fe03
Wes <wp3445@gmail.com>
2023-08-30 15:10:28 -0500

    bug fix

--- client/components/CurrentMenteeList.jsx
+++ client/components/CurrentMenteeList.jsx
@@ -1,23 +1,29 @@
 // ---- TO DO LIST ----
 
 // ---- imports go here ----
-import React, { useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import Mentee from './Mentee.jsx';
 import { useRouteLoaderData } from 'react-router-dom';
 
 // ---- build & export the current mentee list here ----
 const CurrentMenteeList = (props) => {
   const userData = useRouteLoaderData('home');
-  const menteeComponents = [];
+  const [menteeComponents, setMenteeComponents] = useState([]);
 
-  userData.menteeData.forEach((currentMentee) => {
-    menteeComponents.push(
-      <Mentee
-        key={currentMentee.username}
-        menteeName={currentMentee.username}
-      />
-    );
-  });
+  useEffect(() => {
+    if (userData.menteeData) {
+      const newMenteeData = [];
+      userData.menteeData.forEach((currentMentee) => {
+        newMenteeData.push(
+          <Mentee
+            key={currentMentee.username}
+            menteeName={currentMentee.username}
+          />
+        );
+      });
+      setMenteeComponents(newMenteeData);
+    }
+  }, []);
 
   return (
     <div>
